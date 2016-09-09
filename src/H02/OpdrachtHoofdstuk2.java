8b2a93e8b52533bac443d633d4f63ce395dc8e18
mustanis <mtanis@roc-dev.com>
2016-09-09 12:33:09 +0100

    Opdracht 2.1 & 2.2

---
+++ src/H02/OpdrachtHoofdstuk2.java
@@ -0,0 +1,20 @@
+package H02;
+
+/**
+ * Created by Ummah on 9-9-2016.
+ */
+
+import java.awt.*;
+import java.applet.*;
+
+
+public class OpdrachtHoofdstuk2 extends Applet {
+
+    public void init () {
+        setBackground(Color.blue);
+    }
+    public void paint (Graphics g){
+        g.setColor(Color.yellow);
+        g.drawString("Mustafa Tanis",50,60);
+    }
+}
---
+++ src/H02/OpdrachtHoofdstuk202.java
@@ -0,0 +1,22 @@
+package H02;
+
+/**
+ * Created by Ummah on 9-9-2016.
+ */
+
+import java.awt.*;
+import java.applet.*;
+
+
+public class OpdrachtHoofdstuk202 extends Applet {
+
+    public void init (){
+    }
+    public void paint (Graphics g){
+        g.drawString("Mustafa",50,50);
+        g.setColor(Color.red);
+        g.drawString("Tanis",50,60);
+
+    }
+
+}
